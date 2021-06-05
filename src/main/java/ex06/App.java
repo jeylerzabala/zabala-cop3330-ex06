a18bc3ccfec45a9bb33748c65ce26cfb2a702a31
Jeyler desktop <jeylerzabala@gmail.com>
2021-06-05 19:10:14 -0400

    Initial solution to exercise 6

--- src/main/java/ex06/App.java
+++ src/main/java/ex06/App.java
@@ -2,9 +2,12 @@ package ex06;
 
 /*
 Exercise 6 - Retirement Calculator
-Your computer knows what the current year is, which means you can incorporate that into your programs. You just have to figure out how your programming language can provide you with that information.
+Your computer knows what the current year is, which means you can incorporate that into your programs.
+You just have to figure out how your programming language can provide you with that information.
 
-Create a program that determines how many years you have left until retirement and the year you can retire. It should prompt for your current age and the age you want to retire and display the output as shown in the example that follows.
+Create a program that determines how many years you have left until retirement and the year you can retire.
+It should prompt for your current age and the age you want to retire and display the output as shown
+in the example that follows.
 
 Example Output
 What is your current age? 25
@@ -18,5 +21,37 @@ Challenge
 Handle situations where the program returns a negative number by stating that the user can already retire.
 */
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+
+        int currentAge, retirementAge, difference;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What is your current age? ");
+        currentAge = sc.nextInt();
+
+        Scanner sc2 = new Scanner(System.in);
+        System.out.print("At what age would you like to retire? ");
+        retirementAge = sc.nextInt();
+
+        difference = retirementAge - currentAge;
+
+        System.out.println("You have " + difference + " years left until you can retire." );
+
+        Calendar currentDate = Calendar.getInstance();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy");
+        String Date = format.format(currentDate.getTime());
+
+        currentDate.add(Calendar.DATE, difference * 365 );
+        SimpleDateFormat format1 = new SimpleDateFormat("yyyy");
+        String Date1 = format1.format(currentDate.getTime());
+
+        System.out.println("It's " + Date + ", so you can retire in " + Date1 + ".");
+
+    }
 }
