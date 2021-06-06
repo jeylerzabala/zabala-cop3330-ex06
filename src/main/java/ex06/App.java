86ff1cd76a5a10507187d31a8a003629919f0703
Jeyler desktop <jeylerzabala@gmail.com>
2021-06-06 03:20:12 -0400

    Initial solution to exercise 6

--- src/main/java/ex06/App.java
+++ src/main/java/ex06/App.java
@@ -1,6 +1,10 @@
 package ex06;
 
 /*
+
+*  UCF COP3330 Summer 2021 Assignment 1 Solution
+*  Copyright 2021 Jeyler Zabala
+
 Exercise 6 - Retirement Calculator
 Your computer knows what the current year is, which means you can incorporate that into your programs.
 You just have to figure out how your programming language can provide you with that information.
@@ -14,6 +18,7 @@ What is your current age? 25
 At what age would you like to retire? 65
 You have 40 years left until you can retire.
 It's 2015, so you can retire in 2055.
+
 Constraints
 Again, be sure to convert the input to numerical data before doing any math.
 Don’t hard-code the current year into your program. Get it from the system time via your programming language.
@@ -34,7 +39,6 @@ public class App {
         System.out.print("What is your current age? ");
         currentAge = sc.nextInt();
 
-        Scanner sc2 = new Scanner(System.in);
         System.out.print("At what age would you like to retire? ");
         retirementAge = sc.nextInt();
 
